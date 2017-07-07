94aaafa4f118b26dfe368ac3e7978ffc6cf0d94d
Charles Nelson <charlesnelson28@gmail.com>
2017-07-07 15:40:31 -0400

    fix + refactor: remove unused babel deps, fix .babelrc & prepublish.sh

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,5 +1,5 @@
 echo "> Start transpiling"
 echo ""
-./node_modules/.bin/babel --plugins "transform-runtime" src --ignore __tests__ --out-dir ./dist
+./node_modules/.bin/babel src --ignore __tests__ --out-dir ./dist
 echo ""
 echo "> Complete transpiling"
